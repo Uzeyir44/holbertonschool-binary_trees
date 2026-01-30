27cf237844554d0f1a00141b63f0ad40005450ca
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 14:01:40 +0400

    parentheses around &&

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
     left_check = binary_tree_is_full(tree->left);
     right_check = binary_tree_is_full(tree->right);
 
-    if (left_check == 0 && right_check == 0 || left_check == 1 && right_check == 1)
+    if ((left_check == 0 && right_check == 0) || (left_check == 1 && right_check == 1))
         return (1);
     return(0);
 }
