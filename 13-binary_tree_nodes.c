31ccf68f26c8b8c4e44dd692682443d3595eab39
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 13:30:12 +0400

    task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t num = 0;
+
+    if (tree == NULL)
+        return (0)
+
+    num += binary_tree_nodes(tree->left);
+    num += binary_tree_nodes(tree->right);
+
+    if (tree->left != NULL || tree->right != NULL)
+        num += 1;
+
+    return (num)
+}
