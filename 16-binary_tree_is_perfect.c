9d5feca2fe342c3f6c8bc41000d9cb7a35cf10e2
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 14:45:26 +0400

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
+	{
 		return (0);
+	}
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
@@ -26,71 +28,38 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 
 	if (left_height > right_height)
+	{
 		return (left_height + 1);
+	}
 
 	return (right_height + 1);
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree
  *
- * Return: Balance factor, or 0 if tree is NULL
+ * Return: 1 if the tree is perfect, 0 otherwise
  */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int left_height;
-	int right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
-}
-
-/**
- * binary_tree_is_full - Checks if a binary tree is full
- * @tree: Pointer to the root node of the tree
- *
- * Return: 1 if the tree is full, 0 otherwise
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		return (binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right));
-	}
-
-	return (0);
-}
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
+	if (tree == NULL)
+	{
 		return (0);
+	}
 
 	if (tree->left == NULL && tree->right == NULL)
+	{
 		return (1);
+	}
 
 	if (tree->left != NULL && tree->right != NULL)
 	{
 		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+		{
 			return (binary_tree_is_perfect(tree->left) &&
 				binary_tree_is_perfect(tree->right));
+		}
 	}
 
 	return (0);
