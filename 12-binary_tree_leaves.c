0abddbe79ea73a3e9749764d70a222561ff9f5e5
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 13:16:31 +0400

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,2 +1,18 @@
 #include "binary_trees.h"
 #include <stddef.h>
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t num = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    num += binary_tree_leaves(tree->left);
+    num += binary_tree_leaves(tree->right);
+
+    if (tree->left == NULL && tree->right == NULL)
+        num = 1;
+
+    return (num);
+}
