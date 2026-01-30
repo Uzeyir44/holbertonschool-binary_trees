e6089a786c97b2e32f4078bc64f98a8c8b360a6b
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 13:49:13 +0400

    betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,25 +19,34 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->parent == NULL)
 	{
 		if (left_height > right_height)
+		{
 			return (left_height);
+		}
 		return (right_height);
 	}
 
 	if (left_height > right_height)
 		return (left_height + 1);
+
 	return (right_height + 1);
 }
 
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Balance factor, or 0 if tree is NULL
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height;
-    int right_height;
+	int left_height;
+	int right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return (left_height - right_height);
+	return (left_height - right_height);
 }
