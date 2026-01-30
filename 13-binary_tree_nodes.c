44d8775f7f546d31a607c4b5b69a087e206a793c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 13:30:52 +0400

    missed semicolon line 8

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
     size_t num = 0;
 
     if (tree == NULL)
-        return (0)
+        return (0);
 
     num += binary_tree_nodes(tree->left);
     num += binary_tree_nodes(tree->right);
