d3d9d08a3b47a5270510984b39405bda0b202beb
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-30 14:41:17 +0400

    just checking

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -81,9 +81,17 @@ int binary_tree_is_full(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
     if (tree == NULL)
-        return (0);
+		return (0);
 
-    if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree))
-        return (1);
-    return(0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+			return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
+
+	return (0);
 }
